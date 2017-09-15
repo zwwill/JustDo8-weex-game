fe836ca79486c6db04713057122cafc7a09da37d
张威 <wei.zhangwei@corp.netease.com>
2017-09-15 18:53:12 +0800

    框架搭建

---
+++ assets/components/stone.vue
@@ -0,0 +1,60 @@
+<template>
+    <text class="u-stone" :style="{left:x,bottom:y,color:color,backgroundColor:backgroundColor}" v-if="score!=0">{{score}}</text>
+</template>
+
+<style scoped>
+    .u-stone{
+        width: 125px;
+        height: 125px;
+        border-bottom-width: 10px;
+        border-bottom-color: rgba(0,0,0,0.3);
+        line-height: 125px;
+        text-align: center;
+        font-size: 80px;
+        color: #666;
+        background-color: #ddd;
+        position: absolute;
+    }
+</style>
+
+<script>
+
+    export default {
+        props: {
+            p: {
+                type: String,
+                default: "00"
+            },
+            num: {
+                type: Number,
+                default: 1
+            }
+        },
+        data(){
+            return {
+                colors:             ["#666","#666","#eee","#b9e3ee","#ebe94b","#46cafb","#eca48f","#decb3d","#8d1894"],
+                backgroundColors:   ["#ddd","#ddd","#999","#379dc3","#36be0d","#001cc6","#da4324","#56125a","#ffffff"]
+            }
+        },
+        computed: {
+            x: function () {
+                return 125*this.p.charAt(0)+"px";
+            },
+            y: function () {
+                return 125*this.p.charAt(1)+"px";
+            },
+            color: function () {
+                return this.colors[this.score];
+            },
+            backgroundColor: function () {
+                return this.backgroundColors[this.score];
+            },
+            score: function () {
+                return this.num<9&&this.num>0?this.num:0
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
--- src/index.vue
+++ src/index.vue
@@ -1,29 +1,66 @@
 <template>
-  <div class="wrapper" @click="update">
-    <image :src="logoUrl" class="logo"></image>
-    <text class="title">Hello {{target}}</text>
-    <text class="desc">Now, let's use vue to build your weex app.</text>
+  <div class="wrapper" @swipe="onSwipe" @click="onClick" @panstart="onPanstart" @horizontalpan="onHorizontalpan">
+      <image class="i-banner" resize="contain" src="http://doc.zwwill.com/justdo8/img/banner.png?t"></image>
+      <text style="color:#fff;">{{action}}</text>
+      <text style="color:#fff;">{{actionStart+"->"+actionTo}}</text>
+      <div class="stone-box">
+          <stone p="27" num=1></stone>
+      </div>
   </div>
 </template>
 
-<style>
-  .wrapper { align-items: center; margin-top: 120px; }
-  .title { padding-top:40px; padding-bottom: 40px; font-size: 48px; }
-  .logo { width: 360px; height: 156px; }
-  .desc { padding-top: 20px; color:#888; font-size: 24px;}
+<style scoped>
+    .wrapper {
+        background-color: #000;
+        padding-top: 20px;
+        display: flex;
+        flex-wrap: nowrap;
+        flex-direction: column;
+    }
+    .i-banner{
+        width: 400px;
+        height: 100px;
+    }
+    .stone-box{
+        position: relative;
+        flex: 1;
+    }
+
 </style>
 
 <script>
-  export default {
-    data: {
-      logoUrl: 'http://img1.vued.vanthink.cn/vued08aa73a9ab65dcbd360ec54659ada97c.png',
-      target: 'World'
-    },
-    methods: {
-      update: function (e) {
-        this.target = 'Weex'
-        console.log('target:', this.target)
-      }
+
+    //              Nat.audio.play('http://cdn.instapp.io/nat/samples/audio.mp3',() => {
+    //
+    //              });
+    import Nat from 'natjs';
+    var modal = weex.requireModule('modal');
+    import stone from '../assets/components/stone.vue';
+    export default {
+        components: {
+            stone: stone
+        },
+        data: {
+            action: 'action',
+            actionSt:100,
+            actionStart:"*",
+            actionTo:"*"
+        },
+        methods: {
+            onClick: function (e) {
+                this.action = "click";
+            },
+            onSwipe: function (e) {
+                this.action = "swipe:" + e.direction;
+            },
+            onPanstart: function (e) {
+//                this.actionSt++;
+                this.actionStart=e.changedTouches[0].screenX;
+            },
+            onHorizontalpan: function (e) {
+                this.action = "horizontalpan";
+                this.actionTo = e.changedTouches[0].screenX
+            }
+        }
     }
-  }
 </script>
